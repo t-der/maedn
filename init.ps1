cfe0ea37b33b0e88be337b7c018aa4fdb4353f67
My Name <my@mail.me>
2020-05-05 19:53:46 +0200

    added gitignore and init project ps

---
+++ init.ps1
@@ -0,0 +1,7 @@
+﻿# initialize project
+
+# open browser
+Start-Process -FilePath 'chrome.exe' -ArgumentList "--user-data-dir=$PSScriptRoot\.temp", '--start-maximized', "$PSScriptRoot\src\index.html", '--auto-open-devtools-for-tabs'
+
+# open Notepad and files
+Start-Process -FilePath 'notepad++.exe' -ArgumentList "$PSScriptRoot\src\index.html", "$PSScriptRoot\src\scripts\main.js", "$PSScriptRoot\src\scripts\board.js", "$PSScriptRoot\src\style\main.css"
